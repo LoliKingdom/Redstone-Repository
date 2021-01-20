f67a84cf0da2bf4bfa87df863ca6c6a35d9ed381
Rongmario <ethanmahmood@gmail.com>
2021-01-20 05:34:05 +0000

    Update gradle + remove TF dependency

--- src/main/java/thundr/redstonerepository/RedstoneRepository.java
+++ src/main/java/thundr/redstonerepository/RedstoneRepository.java
@@ -34,8 +34,7 @@ public class RedstoneRepository {
     public static final String ID = "redstonerepository";
     public static final String VERSION = "1.3.2";
     public static final String GUIFACTORY = "thundr.redstonerepository.gui.ConfigGuiFactory";
-    public static final String REQUIRED = "required-after:redstonearsenal@[2.3.11,);" + "required-after:cofhcore@[4.3.11,);" +
-            "required-after:thermalfoundation@[2.3.11,);" + "required-after:thermalexpansion@[5.3.11,);" + "after:baubles;";
+    public static final String REQUIRED = "required-after:redstonearsenal@[2.3.11,);" + "required-after:cofhcore@[4.3.11,);" + "required-after:thermalexpansion@[5.3.11,);" + "after:baubles;";
     public static final String CLIENTPROXY = "thundr.redstonerepository.proxies.ClientProxy";
     public static final String COMMONPROXY = "thundr.redstonerepository.proxies.CommonProxy";
     
